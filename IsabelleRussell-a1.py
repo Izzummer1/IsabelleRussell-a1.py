f925105c39738ebec0780b477ddb231955fab665
Izzummer1 <56359459+Izzummer1@users.noreply.github.com>
2019-11-04 19:39:43 -0500

    Add files via upload

---
+++ IsabelleRussell-a1.py
@@ -0,0 +1,24 @@
+candy1 = "Twix"
+candy2 = "KitKat"
+candy3 = "Caramel"
+
+"M == morning"
+
+if 700 == candy1:
+    print("Eat "+str(700)+" Twix")
+
+timeofday = input("700 (M): Twix ")
+
+"A == afternoon"
+
+if 1200 == candy2:
+    print("Eat "+str(1200)+" KitKat")
+
+timeofday = input("1200 (A): KitKat ")
+
+"E == evening"
+
+if 800 == candy3:
+    print("Eat "+str(800)=" Caramel")
+
+timeofday = input("800 (E): Caramel ")
